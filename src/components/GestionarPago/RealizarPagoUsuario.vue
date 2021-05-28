f05c8a13fb12bbffcf895dd6bc1dbd119664c794
William <williammq@outlook.com>
2021-05-27 20:42:58 -0500

    Modificar pago

---
+++ src/components/GestionarPago/RealizarPagoUsuario.vue
@@ -0,0 +1,61 @@
+<template>
+<v-card>
+    <v-card-title class="justify-center">Realizar Pago</v-card-title>
+    <v-stepper v-model="step">
+      <v-stepper-header>
+        <v-stepper-step editable step="1">
+          Datos Generales
+        </v-stepper-step>
+
+        <v-divider></v-divider>
+
+        <v-stepper-step editable step="2">
+          Datos Especificos
+        </v-stepper-step>
+
+      </v-stepper-header>
+
+      <v-stepper-items
+        ><!--CONTIENE LOS STEPPERS CREADOS ARRIBA EN ESTE CASO SON dos-->
+        <v-stepper-content step="1"
+          ><!--CONTIENE EL STEPPERS 1 -->
+          <div class="container-user">
+            <form ></form>
+          </div>
+        </v-stepper-content>
+        <v-stepper-content step="2"
+          ><!--CONTIENE EL STEPPERS 2 -->
+          <div class="container-user">
+            <form>
+            </form>
+          </div>
+        </v-stepper-content>
+      </v-stepper-items>
+  </v-stepper> 
+</v-card>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "RealizarPagoUsuario",
+  data(){
+  return{
+
+
+
+    }
+  },
+  methods:{
+      cerrarDialogo() {
+            this.$emit("close-dialog-Pago");
+      },
+
+  }
+
+}
+</script>
+
+<style>
+
+</style>
