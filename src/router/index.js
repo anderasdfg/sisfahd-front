bcf3b59fc0df32c42762cbbc569c6bbb3cb6d9be
Anderley Candela <anderleyc@gmail.com>
2021-06-11 17:36:12 -0500

    Cambios guapos para la reserva

--- src/router/index.js
+++ src/router/index.js
@@ -82,6 +82,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
+   {
+    path: '/principal',
+    name: 'Principal',    
+    component: () => import('../views/Principal.vue')
+  },
 ]
 
 const router = new VueRouter({
---
+++ src/views/Principal.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="main">
+    <div class="left">
+    <p class="title">Bienvenidos al</p>    
+    <h1>Hospital digital</h1>
+    <button>Reserva tu cita</button>
+   </div>
+  <div class="right">
+      <img
+        src="https://i.ibb.co/hCvzRNK/undraw-medicine-b1ol-1.png"
+        alt=""
+      />
+   </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  data() {
+    return {
+      listaPro: [
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "11",
+        "12",
+        "13",
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+@import "../styles/main.scss";
+.main {
+  font-family: "Nunito", sans-serif;
+  background-color: $sky-light;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  .left {
+
+  }
+  .right {
+    img {
+      width: 50%;
+    }
+  }
+  
+}
+</style>
