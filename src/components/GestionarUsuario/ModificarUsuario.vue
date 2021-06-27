30784aad3d0d8f90fea42593b89c2aa8b482239e
____Gonzales <leicam10@outlook.com>
2021-06-27 01:00:56 -0500

    ai

--- src/components/GestionarUsuario/ModificarUsuario.vue
+++ src/components/GestionarUsuario/ModificarUsuario.vue
@@ -24,42 +24,51 @@
           <div class="container-user">
             <v-text-field
               v-model="usuario.datos.nombre"
-              :counter="10"
               label="Escribe tu nombre"
-              required
+               @input="$v.usuario.datos.nombre.$touch()"
+              @blur="$v.usuario.datos.nombre.$touch()"
+              :error-messages="errorNombre"
             ></v-text-field>
 
             <v-text-field
               v-model="usuario.datos.apellido_paterno"
               label="Escribe tu Apellido Paterno"
-              required
+              @input="$v.usuario.datos.apellido_paterno.$touch()"
+              @blur="$v.usuario.datos.apellido_paterno.$touch()"
+              :error-messages="errorApellidoP"
             ></v-text-field>
 
             <v-text-field
               v-model="usuario.datos.apellido_materno"
               label="Escribe tu Apellido Materno"
-              required
+               @input="$v.usuario.datos.apellido_materno.$touch()"
+              @blur="$v.usuario.datos.apellido_materno.$touch()"
+              :error-messages="errorApellidoM"
             ></v-text-field>
 
             <v-select
               v-model="usuario.datos.tipo_documento"
               :items="itemsTD"
               label="Selecciona un tipo de documento"
-              required
+              :rules="[(v) => !!v || 'Tipo de documento requerido']"
             ></v-select>
 
             <v-text-field
               v-model="usuario.datos.numero_documento"
               :counter="8"
               label="Ingresa tu numero de documento"
-              required
+              @input="$v.usuario.datos.numero_documento.$touch()"
+              @blur="$v.usuario.datos.numero_documento.$touch()"
+              :error-messages="errorNumeroDocumento"
             ></v-text-field>
 
             <v-text-field
               v-model="usuario.datos.telefono"
-              :counter="9"
               label="Ingresa tu numero de celular"
-              required
+              @input="$v.usuario.datos.telefono.$touch()"
+              @blur="$v.usuario.datos.telefono.$touch()"
+              :error-messages="errorTelefono"
+              :required="true"
             ></v-text-field>
 
             <v-menu
@@ -77,6 +86,10 @@
                   readonly
                   v-bind="attrs"
                   v-on="on"
+                  :error-messages="errorFechaNacimiento"
+                  @input="$v.usuario.datos.fecha_nacimiento.$touch()"
+                  @blur="$v.usuario.datos.fecha_nacimiento.$touch()"
+                  :required="true"
                   color="#009900"
                   outlined
                   label="Fecha de tu nacimiento"
@@ -92,14 +105,17 @@
             <v-text-field
               v-model="usuario.datos.correo"
               label="Ingresa tu correo electronico"
-              required
+               :error-messages="errorCorreo"
+              @input="$v.usuario.datos.correo.$touch()"
+              @blur="$v.usuario.datos.correo.$touch()"
+              :required="true"
             ></v-text-field>
 
             <v-select
               v-model="usuario.datos.sexo"
               :items="itemsS"
+              :rules="[(v) => !!v || 'Sexo requerido']"
               label="Selecciona tu sexo"
-              required
             ></v-select>
 
             <!-- <v-text-field
@@ -133,7 +149,10 @@
             <v-text-field
               v-model="usuario.usuario"
               label="Confirma tu correo"
-              required
+              :error-messages="errorUsuario"
+              @input="$v.usuario.usuario.$touch()"
+              @blur="$v.usuario.usuario.$touch()"
+              :required="true"
             ></v-text-field>
 
             <v-text-field
@@ -142,8 +161,10 @@
               :type="show1 ? 'text' : 'password'"
               name="input-10-1"
               label="Escribe tu contraseña"
-              hint="At least 8 characters"
-              counter
+              :error-messages="errorClave"
+              @input="$v.usuario.clave.$touch()"
+              @blur="$v.usuario.clave.$touch()"
+              :required="true"
               @click:append="show1 = !show1"
             ></v-text-field>
           </div>
@@ -186,6 +207,13 @@
 <script>
 import axios from "axios";
 import { mapMutations } from "vuex";
+import { required, minLength, email, numeric} from "vuelidate/lib/validators";
+
+// function esContraseña(value) {
+//   //Minimum eight characters, at least one letter and one number:
+//   return /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,60}$/.test(value); 
+// }
+
 export default {
   props: ["usuario"],
   data() {
@@ -246,6 +274,172 @@ export default {
         })
         .catch((err) => console.log(err));
     },
+  },
+
+  computed: {
+    errorNombre() {
+      const errors = [];
+      if (!this.$v.usuario.datos.nombre.$dirty) return errors;
+      !this.$v.usuario.datos.nombre.required &&
+        errors.push("Debe ingresar el nombre del usuario");
+      !this.$v.usuario.datos.nombre.minLength &&
+        errors.push("El nombre del usuario debe poseer al menos 7 caracteres");
+
+      return errors;
+    },
+    errorApellidoP() {
+      const errors = [];
+      if (!this.$v.usuario.datos.apellido_paterno.$dirty) return errors;
+      !this.$v.usuario.datos.apellido_paterno.required &&
+        errors.push("Debe ingresar el apellido paterno del usuario paciente");
+      !this.$v.usuario.datos.apellido_paterno.minLength &&
+        errors.push(
+          "El apellido paterno del usuario debe poseer al menos 7 caracteres"
+        );
+
+      return errors;
+    },
+    errorApellidoM() {
+      const errors = [];
+      if (!this.$v.usuario.datos.apellido_materno.$dirty) return errors;
+      !this.$v.usuario.datos.apellido_materno.required &&
+        errors.push("Debe ingresar el apellido materno del usuario paciente");
+      !this.$v.usuario.datos.apellido_materno.minLength &&
+        errors.push(
+          "El apellido materno del usuario debe poseer al menos 7 caracteres"
+        );
+
+      return errors;
+    },
+
+    errorNumeroDocumento() {
+      const errors = [];
+      if (!this.$v.usuario.datos.numero_documento.$dirty) return errors;
+      !this.$v.usuario.datos.apellido_materno.required &&
+        errors.push(
+          "Debe ingresar el numero de documento del usuario paciente"
+        );
+      !this.$v.usuario.datos.apellido_materno.minLength &&
+        errors.push("El numero de documento debe poseer 8 caracteres");
+
+      return errors;
+    },
+
+    errorTelefono() {
+      const errors = [];
+      if (!this.$v.usuario.datos.telefono.$dirty) return errors;
+      !this.$v.usuario.datos.telefono.required &&
+        errors.push("El campo no puede estar en blanco");
+      !this.$v.usuario.datos.telefono.numeric &&
+        errors.push("Ingrese solo numeros válidos");
+      return errors;
+    },
+
+    errorFechaNacimiento() {
+      const errors = [];
+      if (!this.$v.usuario.datos.fecha_nacimiento.$dirty) return errors;
+      !this.$v.usuario.datos.fecha_nacimiento.required &&
+        errors.push("El campo no puede estar en blanco");
+      return errors;
+    },
+
+    errorCorreo() {
+      const errors = [];
+      if (!this.$v.usuario.datos.correo.$dirty) {
+        return errors;
+      }
+      !this.$v.usuario.datos.correo.email &&
+        errors.push("Ingrese una dirección de correo válida");
+      !this.$v.usuario.datos.correo.required &&
+        errors.push("El campo de correo no puede estar en blanco");
+      return errors;
+    },
+
+    errorUsuario() {
+      const errors = [];
+      if (!this.$v.usuario.usuario.$dirty) {
+        return errors;
+      }
+      !this.$v.usuario.usuario.email &&
+        errors.push("Ingrese una dirección de correo válida");
+      !this.$v.usuario.usuario.required &&
+        errors.push("El campo de correo no puede estar en blanco");
+      return errors;
+    },
+
+    errorClave() {
+      const errors = [];
+      if (!this.$v.usuario.clave.$dirty) {
+        return errors;
+      }
+      !this.$v.usuario.clave.required &&
+        errors.push("El campo de contrasena no puede estar en blanco");
+      // !this.$v.usuario.clave.esContraseña &&
+      //   errors.push(
+      //     "Debe tener como mínimo 8 caracteres, con almenos una letra y un numero"
+      return errors;
+    },
+  },
+  validations() {
+    return {
+      usuario: {
+        datos: {
+          nombre: {
+            required,
+            minLength: minLength(3),
+          },
+          apellido_paterno: {
+            required,
+            minLength: minLength(3),
+          },
+          apellido_materno: {
+            required,
+            minLength: minLength(3),
+          },
+          tipo_documento: {
+            required,
+          },
+          numero_documento: {
+            required,
+            minLength: minLength(8),
+          },
+          telefono: {
+            required,
+            minLength: minLength(9),
+            numeric,
+          },
+
+          fecha_nacimiento: {
+            required,
+          },
+
+          correo: {
+            required,
+            email,
+          },
+
+          sexo: {
+            required,
+          },
+
+          //   correo: '',
+          //  correoRules: [
+          //    v => !!v || 'El correo es requerido',
+          //   v => /.+@.+\..+/.test(v) || 'Ingrese un correo valido',
+          //              ],
+        },
+
+        usuario: {
+          required,
+          email,
+        },
+
+        clave: {
+          required,
+          // esContraseña
+        },
+      },
+    };
   },
 };
 </script>
