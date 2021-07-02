8a8acd9a62cddf14e07c270eb4112fa0944480af
____Gonzales <leicam10@outlook.com>
2021-07-01 21:51:05 -0500

    modificar perfil avance

--- src/components/ModificarPerfil/ModificarPerfilPaciente.vue
+++ src/components/ModificarPerfil/ModificarPerfilPaciente.vue
@@ -1,35 +1,102 @@
 <template>
   <v-card elevation="3" class="card-modificarPerfil">
-    <div class="top-card">
-
-      <h1> Modificar Perfil
-      </h1>
-      <img
-        src="https://images.clarin.com/2015/03/20/claima20150320-8312-thinkstock-photos-mujer___HkwKuZR7x_340x340.jpg"
-        alt="Perfil"
-        class="perfil"
-      />
-      <div class="info-paciente">
-        <h2>{{this.user.datos.nombre}} {{this.user.datos.apellido_paterno}} {{this.user.datos.apellido_materno }}</h2>
-        <p>{{this.user.datos.fecha_nacimiento}}</p>
-        <p>{{this.user.datos.telefono}}</p>
-        <p>{{this.user.datos.correo}}</p>
+    <div class="top-card" v-if="this.user">
+      <h1 class="titulo">Modificar Perfil Paciente</h1>
+      <div class="image">
+        <img
+          :src="
+            this.user.datos.foto
+              ? this.user.datos.foto
+              : 'https://image.flaticon.com/icons/png/512/2741/2741191.png'
+          "
+          alt="Perfil"
+          class="image"
+        />
       </div>
-      <button class="button-little">Historia clínica</button>
+
+      <div class="info-paciente">
+        <h3 class="font-weight-medium subtitulo">Nombre</h3>
+        <p>{{ this.user.datos.nombre }}</p>
+        <h3 class="font-weight-medium subtitulo">Apellido paterno</h3>
+        <p>{{ this.user.datos.apellido_paterno }}</p>
+        <h3 class="font-weight-medium subtitulo">Apellido materno</h3>
+        <p>{{ this.user.datos.apellido_materno }}</p>
+        <h3 class="font-weight-medium subtitulo">Tipo de documento</h3>
+        <p>{{ this.user.datos.tipo_documento }}</p>
+        <h3 class="font-weight-medium subtitulo">Numero de documento</h3>
+        <p>{{ this.user.datos.numero_documento }}</p>
+        <h3 class="font-weight-medium subtitulo">Telefono</h3>
+        <p>{{ this.user.datos.telefono }}</p>
+        <h3 class="font-weight-medium subtitulo">Fecha de nacimiento</h3>
+        <p>{{ this.user.datos.fecha_nacimiento }}</p>
+        <h3 class="font-weight-medium subtitulo">Correo</h3>
+        <p>{{ this.user.datos.correo }}</p>
+        <h3 class="font-weight-medium subtitulo">Sexo</h3>
+        <p>{{ this.user.datos.sexo }}</p>
+
+        <!-- <button class="button-little">Modificar perfil</button> -->
+
+        <v-row align="center" justify="space-around">
+          <v-btn
+            class="ma-2"
+            outlined
+            color="indigo"
+            @click="abrirDialogoModificarPerfilPaciente()"
+          >
+            Modificar
+          </v-btn>
+        </v-row>
+      </div>
     </div>
-    <div class="info-adicional">         
-        
-          <p><b>Peso (kg) </b>54</p>
-          <p><b>Grupo sanguíneo </b>O+</p>
-        
-        
-          <p><b>Talla (m) </b>1.45</p>
-          <p><b>Edad </b>38 años</p>
-        
-    </div>
+<v-dialog v-model="dialogoModificarPerfiPaciente" high="200" width="400px">
+        <v-card outlined>
+          <v-card-title class="text-h5 grey lighten-2">
+            <h3>Seleccion de rol</h3>
+          </v-card-title>
+
+          <v-card-text>
+            Seleccione el rol del usuario a registrar
+
+            <v-combobox
+              solo
+              :items="misItems"
+              v-model="miRol"
+              label="Rol del usuario"
+            >
+            </v-combobox>
+          </v-card-text>
+
+          <v-divider> </v-divider>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="error" text dark @click="dialogoregistro = false">
+              Cerrar
+            </v-btn>
+
+            <v-btn color="success" dark class="mb-2" @click="rolSelecionado">
+              <span>Registrar nuevo Usuario</span>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+     
   </v-card>
 </template>
 
+<script>
+export default {
+  name: "ModificarPerfilPaciente",
+  props: ["user"],
+  data() {
+    return {};
+  },
+
+  methods: {
+    async modificarPerfilPaciente() {},
+  },
+};
+</script>
 <style lang="scss">
 // .home {
 //   margin: 1%;
@@ -37,7 +104,47 @@
 //   justify-content: space-around;
 // }
 .card-modificarPerfil {
-  max-width: 80%;
-  border-radius: 20px !important;
+  // max-width: 100%;
+  // justify-content: space-around;
+  // // border-radius: 20px !important;
+
+  padding: 3%;
+  border-radius: 20px;
+  height: 100%;
+}
+.image {
+  margin-left: auto;
+  margin-right: auto;
+  display: block;
+}
+.info-paciente {
+  h2 {
+    font-size: 24px;
+  }
+  p {
+    margin-bottom: 1%;
+  }
+
+  h3 {
+    font-size: 15px;
+  }
+}
+
+.titulo {
+  font-size: 1.5rem;
+  color: $blue;
+  font-weight: bold;
+}
+.subtitulo {
+  font-size: 1.2rem;
+  color: $blue;
+}
+.button-little {
+  background: $sky-light;
+  color: $blue;
+  border-radius: 6px;
+  height: 5vh;
+  padding: 1.5%;
+  text-align: center;
 }
 </style>
--- src/views/ModificarPerfil/ModificarPerfil.vue
+++ src/views/ModificarPerfil/ModificarPerfil.vue
@@ -1,127 +1,95 @@
 <template>
-
-  <div class="home">
-    <ModificarPerfilPaciente :user="this.user" class="card-modificarPerfil" />
-    <v-card elevation="3" class="card">
-      <div class="top-card">
-      <img
-        src="https://images.clarin.com/2015/03/20/claima20150320-8312-thinkstock-photos-mujer___HkwKuZR7x_340x340.jpg"
-        alt="Perfil"
-        class="perfil"
-      />
-      
-      <div class="info-paciente">
-      <h1> Datos del usuario </h1>
-
-      <p> Nombre: </p>
-      <p> Apellido paterno: </p>
-      <p> Apellido materno: </p>
-      <p> Tipo de documento: </p>
-      <p> Numero de documento: </p>
-      <p> Telefono: </p>
-      <p> Fecha de nacimiento: </p>
-      <p> Correo: </p>
-      <p> Sexo: </p>
+  <div>
+    <div v-if="this.user">
+      <div class="home" v-if="this.user.rol == '607f37c1cb41a8de70be1df3'">
+        <ModificarPerfilPaciente :user="this.user" class="card-datitos" />
       </div>
 
-      <button class="button-little">Modificar perfil</button>
-
-</div>
-
-    
-
-    </v-card>
+      <div class="home" v-if="this.user.rol == '607f2beacb41a8de70be1dec'">
+        <ModificarPerfilMedico :user="this.user" class="card-datitos" />
+      </div>
+    </div>
   </div>
-
-  
 </template>
 
-<style lang="scss" scoped>
-.perfil {
-  width: 20%;
-  height: 20%;
-  border-radius: 20px;
-}
-</style>
 
 <script>
 import ModificarPerfilPaciente from "@/components/ModificarPerfil/ModificarPerfilPaciente.vue";
+import ModificarPerfilMedico from "@/components/ModificarPerfil/ModificarPerfilMedico.vue";
+import { mapGetters, mapActions, mapState, mapMutations } from "vuex";
 
 export default {
   name: "ModificarPerfil",
   components: {
     ModificarPerfilPaciente,
+    ModificarPerfilMedico,
   },
-  data(){ 
+  data() {
     return {
-
       rules: [
-        value => !!value || 'Required.',
-        value => (value && value.length >= 3) || 'Min 3 characters',
+        (value) => !!value || "Required.",
+        (value) => (value && value.length >= 3) || "Min 3 characters",
       ],
       //
-    }
+    };
   },
-//   computed: {
-//     ...mapState(["drawer"]),
-//     ...mapGetters(["user"]),
-//   },
-//   created() {
-//     this.fetchUser();    
-//   },
-//   methods: {
-//     ...mapActions(["fetchUser"]),    
-//   },
-}
-
+    computed: {
+      ...mapState(["drawer"]),
+      ...mapGetters(["user"]),
+    },
+  //   created() {
+  //     this.fetchUser();
+  //   },
+  //   methods: {
+  //     ...mapActions(["fetchUser"]),
+  //   },
+};
 </script>
 
 <style lang="scss" scoped>
-  .perfil {
-    width: 25%;
-    height: 25%;
-    border-radius: 20px;
+.perfil {
+  width: 25%;
+  height: 25%;
+  border-radius: 20px;
+}
+.card {
+  padding: 1%;
+  border-radius: 20px;
+}
+.info-paciente {
+  h2 {
+    font-size: 24px;
   }
-  .card {
-    padding: 1%;
-    border-radius: 20px;
-    
+  p {
+    margin-bottom: 1%;
   }
-  .info-paciente {
-    h2 {
-      font-size: 24px;
-    }
-    p {
-      margin-bottom: 1%;
-    }
-  }
-  .top-card {
-    display: flex;    
-    justify-content: space-between;
-    align-items: flex-start;
-
-  }
-  .info-adicional {
-    margin-top: 3%;
-    display: flex;
-    justify-content: space-around;
-    align-content: space-around;
-  }
-  .button-little {
-    background: $sky-light;
-    color: $blue; 
-    border-radius: 6px;    
-    height: 5vh;
-    padding: 1.5%;
-    text-align: center;
-  }
+}
+.top-card {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+}
+.info-adicional {
+  margin-top: 3%;
+  display: flex;
+  justify-content: space-around;
+  align-content: space-around;
+}
+.button-little {
+  background: $sky-light;
+  color: $blue;
+  border-radius: 6px;
+  height: 5vh;
+  padding: 1.5%;
+  text-align: center;
+}
 
 .home {
   margin: 1%;
   display: flex;
   justify-content: space-around;
 }
-.card-modificarPerfil {
+.card-datitos {
   max-width: 60%;
   border-radius: 20px !important;
 }
