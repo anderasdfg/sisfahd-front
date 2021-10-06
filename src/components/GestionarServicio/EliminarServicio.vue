f9f4aec4b589638808dc1273743bbf7b9df23518
Alonso159 <alonsocastro_20@hotmail.com>
2021-10-06 18:30:46 -0500

    Falta la bd >:v

---
+++ src/components/GestionarServicio/EliminarServicio.vue
@@ -0,0 +1,223 @@
+<template>
+  <v-card>
+    <v-card-title class="justify-center">Eliminar Tarifa</v-card-title>
+
+    <div class="container-EliminarTarifa">
+      <form>
+        <v-text-field
+        label="descripcion"
+        class="campos"
+        v-model="Tarifa4.descripcion" 
+        readonly
+      ></v-text-field>
+         <v-text-field
+        label="Impuesto"
+        class="campos"
+        v-model="Tarifa4.impuesto" 
+        readonly
+      ></v-text-field>
+         <v-text-field
+        label="Subtotal"
+        class="campos"
+        v-model="Tarifa4.subtotal" 
+        readonly
+      ></v-text-field>
+          
+         <v-text-field
+        label="Precio "
+        class="campos"
+        v-model.number="Tarifa4.precio_final" 
+        readonly
+      ></v-text-field>
+         <!--Para archivos :3 -->
+      
+
+        <v-divider class="divider-custom"></v-divider>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-col cols="12" sm="6" md="6">
+            <v-btn block color="success" elevation="2" @click="eliminarTarifa">
+              <v-icon left>mdi-content-save-all-outline</v-icon>
+              <span>Eliminar Tarifa</span>
+            </v-btn>
+          </v-col>
+          <v-col cols="12" sm="6" md="6">
+            <v-btn color="error" elevation="2" block @click="closeDialog">
+              <v-icon left>mdi-close-outline</v-icon>
+              Cerrar
+            </v-btn>
+          </v-col>
+        </v-card-actions>
+      </form>
+    </div>
+    <v-dialog width="450px" v-model="cargaRegistro" persistent>
+      <v-card height="300px">
+        <v-card-title class="justify-center">Eliminando la tarifa</v-card-title>
+        <div>
+          <v-progress-circular
+            style="display: block; margin: 40px auto"
+            :size="90"
+            :width="9"
+            color="purple"
+            indeterminate
+          ></v-progress-circular>
+        </div>
+        <v-card-subtitle
+          class="justify-center"
+          style="font-weight: bold; text-align: center"
+          >En unos momentos finalizaremos...</v-card-subtitle
+        >
+      </v-card>
+    </v-dialog>
+  </v-card>
+</template>
+<script>
+import axios from "axios";
+
+import { mapMutations, mapState,mapGetters } from "vuex";
+import { required, minLength, between } from "vuelidate/lib/validators";
+export default {
+  props: ["Tarifa4"],
+    data() {
+    return {
+      
+      EspecialidadAux: [],
+      cargaRegistro: false
+    };
+  },
+  
+  methods: {
+    
+    async eliminarTarifa() {
+      //let tarifas={descripcion:this.Tarifa4.descripcion,subtotal:this.Tarifa4.subtotal,impuesto:this.Tarifa4.impuesto,id:this.Tarifa4.id,precio_final:this.Tarifa4.precio_final,id_Medico:this.Tarifa4.id_Medico};
+     
+      this.$v.$touch();
+      if (this.$v.$valid) {
+        this.mensaje(
+          "error",
+          "..Oops",
+          "Se encontraron errores en el formulario",
+         
+          false
+        );
+      } else {
+      //  this.cargaRegistro = true;
+      this.Tarifa4.id_Medico=this.user.id;
+      
+      /*console.log(this.user.id_Medico);
+     console.log(this.Tarifa4.id_Medico);*/
+     console.log(this.user.id);
+   
+     
+     
+      await axios
+          .delete("/Tarifa/tarifasmedico/Delete?id="+this.Tarifa4.id)
+          .then((res) => { 
+            this.Especialidad = res.data;
+            if (this.Tarifa4.id !== "") {
+              this.cargaRegistro = false;
+              this.closeDialog();     
+              this.$emit("emit-obtener-tarifas");
+              this.mensaje(
+                "success",
+                "Listo",
+                "Tarifa eliminada satisfactoriamente",
+                "<strong>Se redirigirá a la Interfaz de Gestión<strong>",
+                true
+              );
+            }
+          })
+          .catch((err) => console.log(err));
+     }
+    },
+   
+    mensaje(icono, titulo, texto, footer, valid) {
+      this.$swal({
+        icon: icono,
+        title: titulo,
+        text: texto,
+        footer: footer,
+      }).then((res) => {
+        if (valid) {
+          this.$emit("modifier-Complete");
+        }
+      });
+    },
+    closeDialog() {
+      this.$emit("close-dialog-eliminar");
+    },
+  },
+  computed: {
+     ...mapGetters(['user']),
+   
+    errorNombre() {
+      const errors = [];
+      if (!this.$v.Tarifa4.nombre.$dirty) return errors;
+      if (!this.$v.Tarifa4.nombre) this.errors.push('El nombre es obligatorio.');
+            !this.$v.Tarifa4.nombre.minLength &&
+        errors.push("El nombre de la especialidad debe poseer al menos7 caracteres");
+      return errors;
+    },
+    errorCodigo() {
+      const errors = [];
+      if (!this.$v.Tarifa4.codigo.$dirty) return errors;
+            !this.$v.Tarifa4.codigo.minLength &&
+        errors.push("El codigo de la especialida debe poseer al menos 6 caracteres");
+      return errors;
+    },
+    errorDescripcion() {
+      const errors = [];
+      if (!this.$v.Tarifa4.descripcion.$dirty) return errors;
+           !this.$v.Tarifa4.descripcion.minLength &&
+        errors.push("La descripción debe poseer al menos 7 caracteres");
+      return errors;
+    },
+
+  },
+  
+  validations() {
+    return {
+     /* residente: {
+        id: {
+          required,
+        },
+      },*/
+      Tarifa4: {
+        descripcion: {
+          required,
+          minLength: minLength(7),
+        },
+        nombre: {
+          required,
+          minLength: minLength(8),
+        },
+        codigo: {
+          required,
+          minLength: minLength(6),
+        },
+      },
+      EspecialidadAux: {
+        required,
+      },
+    };
+  },
+};
+</script>
+
+<style  scoped>
+.container-EliminarTarifa {
+  margin: 15px;
+}
+
+.subtitle {
+  color: #314b5f;
+}
+.divider-custom {
+  margin-top: 7px;
+  margin-bottom: 7px;
+}
+
+.inputTextField {
+  border-color: green;
+}
+</style>
---
+++ src/components/GestionarServicio/RegistrarServicio.vue
@@ -0,0 +1,366 @@
+<template>
+  <v-card>
+    <v-card-title class="justify-center">Registrar Servicio</v-card-title>
+    <div class="container-Servicio">
+      <v-form>
+        <v-text-field
+          v-model.trim="Servicio.titulo"
+          label="titulo"
+          outlined
+          @input="$v.Servicio.titulo.$touch()"
+          @blur="$v.Servicio.titulo.$touch()"
+          :error-messages="errortitulo"
+          color="#009900"
+        ></v-text-field>
+
+        <v-text-field
+          v-model.trim="Servicio.descripcion"
+          label="descripcion"
+          outlined
+          @input="$v.Servicio.descripcion.$touch()"
+          @blur="$v.Servicio.descripcion.$touch()"
+          :error-messages="errordescripcion"
+          color="#009900"
+        ></v-text-field>
+        <v-textarea
+          v-model.number="Servicio.monto"
+          label="monto"
+          @input="$v.Servicio.monto.$touch()"
+          @blur="$v.Servicio.monto.$touch()"
+          height="25"
+          rows="2"
+          :error-messages="errormonto"
+          outlined
+          color="#009900"
+        ></v-textarea>
+
+        <vue-dropzone
+          ref="myVueDropzone"
+          @vdropzone-success="afterSuccess"
+          @vdropzone-removed-file="afterRemoved"
+          id="dropzone"
+          :options="dropzoneOptions"
+        >
+        </vue-dropzone>
+
+        <v-alert v-if="errorImagen" color="red">
+          <v-card-text class="mt-2" style="color: white"
+            >Seleccione el archivo respectivo o arrastrelo aqui</v-card-text
+          >
+        </v-alert>
+
+        <v-divider class="divider-custom"></v-divider>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-col cols="12" sm="6" md="6">
+            <v-btn
+              block
+              color="success"
+              elevation="2"
+              @click.prevent="RegistrarServicio"
+              >Registrar</v-btn
+            >
+          </v-col>
+          <v-col cols="12" sm="6" md="6">
+            <v-btn color="error" elevation="2" block @click="closeDialog"
+              >Volver</v-btn
+            >
+          </v-col>
+        </v-card-actions>
+      </v-form>
+    </div>
+    <v-dialog width="450px" v-model="cargaRegistro" persistent>
+      <v-card height="300px">
+        <v-card-title class="justify-center">Registrando Servicio</v-card-title>
+        <div>
+          <v-progress-circular
+            style="display: block;margin:40px auto;"
+            :size="90"
+            :width="9"
+            color="blue"
+            indeterminate
+          ></v-progress-circular>
+        </div>
+        <v-card-subtitle
+          class="justify-center"
+          style="font-weight:bold;text-align:center"
+          >En unos momentos finalizaremos...</v-card-subtitle
+        >
+      </v-card>
+    </v-dialog>
+  </v-card>
+</template>
+
+<script>
+import axios from "axios";
+
+import vue2Dropzone from "vue2-dropzone";
+import "vue2-dropzone/dist/vue2Dropzone.min.css";
+import { mapMutations, mapState } from "vuex";
+import { required, minLength, decimal } from "vuelidate/lib/validators";
+export default {
+  name: "RegistrarServicio",
+  props: ["Servicio"],
+  components: {
+    vueDropzone: vue2Dropzone,
+  },
+  data() {
+    return {
+      step: 1,
+      dropzoneOptions: {
+        url: "https://httpbin.org/post",
+        thumbnailWidth: 250,
+        maxFilesize: 3.0,
+        maxFiles: 1,
+        acceptedFiles: ".jpg, .png, jpeg",
+        headers: { "My-Awesome-Header": "header value" },
+        addRemoveLinks: true,
+        dictDefaultMessage:
+          "Seleccione una Imagen de su Dispositivo o Arrastrela Aqui",
+      },
+
+      Servicio: {
+        titulo: "",
+        descripcion: "",
+        monto: "",
+        url: "",
+      },
+
+      cargaRegistro: false,
+    };
+  },
+
+  watch: {
+    dialog(val) {
+      val || this.close();
+    },
+  },
+  methods: {
+    ...mapMutations(["setE"]),
+
+    dropzone() {
+      var file = {
+        size: 123,
+        name: "Imagen de Servicio",
+        type: "image/jpg",
+      };
+      this.$refs.myVueDropzone.manuallyAddFile(
+        file,
+        this.Servicio.url,
+        null,
+        null,
+        true
+      );
+    },
+    afterSuccess(file, response) {
+      console.log(file);
+      this.Servicio.url = file.dataURL.split(",")[1];
+      this.$v.Servicio.url.$model = file.dataURL.split(",")[1];
+      //console.log(file.dataURL.split(",")[1]);
+    },
+
+    afterRemoved(file, error, xhr) {
+      this.Servicio.url = "";
+      this.$v.Servicio.url.$model = "";
+    },
+    mensaje(icono, titulo, texto, footer, valid) {
+      this.$swal({
+        icon: icono,
+        title: titulo,
+        text: texto,
+        footer: footer,
+      }).then((res) => {
+        if (valid) {
+          this.$emit("modifier-complete");
+        }
+      });
+    },
+    resetServicioValidationState() {
+      this.$refs.myVueDropzone.removeAllFiles();
+      this.$v.Servicio.$reset();
+    },
+    closeDialog() {
+      this.Servicio = this.limpiarServicio();
+      this.step = 1;
+      this.resetServicioValidationState();
+      this.$emit("close-dialog-Registrar");
+    },
+    close() {
+      this.dialog = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    async RegistrarServicio() {
+      this.Servicio.titulo = this.Servicio.titulo;
+      this.Servicio.descripcion = this.Servicio.descripcion;
+      this.Servicio.monto = this.Servicio.monto;
+
+      console.log(this.Servicio);
+      this.$v.Servicio.$touch();
+      //if (this.$v.informe.$invalid) {
+      if (this.$v.$invalid) {
+        this.mensaje(
+          "error",
+          "..Oops",
+          "Se encontraron errores en el formulario",
+
+          false
+        );
+      } else {
+        console.log("no hay errores");
+        this.cargaRegistro = true;
+        await axios
+          .post("/Servicio/Registrar", this.Servicio)
+          .then((res) => {
+            this.Servicio = res.data;
+            this.$emit("emit-obtener-Servicioes");
+            this.cargaRegistro = false;
+            this.closeDialog();
+          })
+          .catch((err) => console.log(err));
+
+        /*await this.mensaje(
+          "success",
+          "Listo",
+          "Turno registrado satisfactoriamente",
+          "<strong>Se redirigira a la interfaz de gestionar turnos<strong>"
+        );*/
+      }
+    },
+
+    limpiarServicio() {
+      return {
+        Servicio: {
+          titulo: "",
+          descripcion: "",
+          monto: "",
+          url: "",
+        },
+      };
+    },
+  },
+
+  async mensaje(icono, titulo, texto, footer) {
+    await this.$swal({
+      icon: icono,
+      title: titulo,
+      text: texto,
+      footer: footer,
+    });
+  },
+  computed: {
+    errortitulo() {
+      const errors = [];
+      if (!this.$v.Servicio.titulo.$dirty) return errors;
+      !this.$v.Servicio.titulo.required &&
+        errors.push("Debe ingresar el titulo de la Servicio");
+      !this.$v.Servicio.titulo.minLength &&
+        errors.push(
+          "El titulo de la Servicio debe poseer al menos 6 caracteres"
+        );
+
+      return errors;
+    },
+    errordescripcion() {
+      const errors = [];
+      if (!this.$v.Servicio.descripcion.$dirty) return errors;
+      !this.$v.Servicio.descripcion.required &&
+        errors.push("Debe ingresar el descripcion de la Servicio");
+      !this.$v.Servicio.descripcion.minLength &&
+        errors.push(
+          "El descripcion de la especialida debe poseer al menos 3 caracteres"
+        );
+      return errors;
+    },
+    errormonto() {
+      const errors = [];
+      if (!this.$v.Servicio.monto.$dirty) return errors;
+      !this.$v.Servicio.monto.required &&
+        errors.push("Debe ingresar la monto del Servicio");
+      !this.$v.Servicio.monto.minLength &&
+        errors.push("La descripción debe poseer al menos 3 caracteres");
+      !this.$v.Servicio.monto.decimal &&
+        errors.push("El Monto del Servicio solo debe poseer numeros");
+      return errors;
+    },
+    errorImagen() {
+      return this.$v.Servicio.url.required == false &&
+        this.$v.Servicio.url.$dirty == true
+        ? true
+        : false;
+    },
+  },
+  validations() {
+    return {
+      Servicio: {
+        titulo: {
+          required,
+          minLength: minLength(6),
+        },
+        descripcion: {
+          required,
+          minLength: minLength(7),
+        },
+        monto: {
+          required,
+          minLength: minLength(3),
+          decimal,
+        },
+        url: {
+          required,
+        },
+      },
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.title-card {
+  font-size: 25px;
+  color: $blue;
+  padding-top: 7%;
+  text-align: center;
+}
+.estilo-stepper {
+  padding-top: 2%;
+}
+.v-dialog .v-card .v-card__title {
+  font-size: 25px;
+  font-weight: bold;
+}
+.campos {
+  margin: 2% 10% 5% 10%;
+}
+.filas {
+  margin: 2% 8% 5% 8%;
+}
+.filas2 {
+  margin-top: 4%;
+  margin-bottom: 1%;
+}
+.btn-registrar {
+  background: $green;
+  color: white;
+  border-radius: 10px;
+  width: 90%;
+  height: 5vh;
+  font-weight: bold;
+  font-size: 20px;
+}
+.container-Servicio {
+  margin: 15px;
+}
+.btn-volver {
+  background: $blue;
+  color: white;
+  border-radius: 10px;
+  width: 90%;
+  height: 5vh;
+  font-weight: bold;
+  font-size: 20px;
+}
+</style>
