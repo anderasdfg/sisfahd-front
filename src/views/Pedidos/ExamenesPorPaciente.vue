9aa4bf6ecefb91dcf604dcb3524ccf91de1e0f57
JoseParedesM <jalejandro04072000@gmail.com>
2021-11-18 01:27:21 -0500

    Cambios para el medico en las Servicios

--- src/views/Pedidos/ExamenesPorPaciente.vue
+++ src/views/Pedidos/ExamenesPorPaciente.vue
@@ -113,11 +113,11 @@
     <v-card-text>
         <v-card elevation="0" outlined shaped>
           <v-card-title class="subtitulo">Pedidos Pendientes</v-card-title>
-          <template v-if="listaPedidos.length > 0">
+          <template v-if="listExamenes.length > 0">
             <div>
               <v-data-table
                 :headers="headerPedidos"
-                :items="listaPedidos"
+                :items="listExamenes"
                 :items-per-page="5"
                 :page.sync="page"
                 @page-count="pageCount = $event"
@@ -131,7 +131,7 @@
                   {{ item.estado_pago | capitalize}}
               </template>
               <template v-slot:[`item.actions`]="{ item }">
-            <v-tooltip top v-if="!(item.estado_pago == 'en proceso')">
+            <v-tooltip top v-if="item.procedimientos.estado == 'No subido'">
             <template v-slot:activator="{ on, attrs }">
               <v-btn
                 class="mx-0"
@@ -140,16 +140,17 @@
                 color="green"
                 v-bind="attrs"
                 v-on="on"
-                @click="atenderPedido(item)"
+                @click="abrirDialodoConfirmar(item)"
               >
-                <v-icon dark>
+                <v-icon dark left>
                   mdi-check
                 </v-icon>
+                <span>Confirmar</span>
               </v-btn>
             </template>
-            <span>Atender pedidos.</span>
+            <span>Confirmar atención.</span>
           </v-tooltip>
-          <v-tooltip top>
+          <v-tooltip top v-if="item.procedimientos.estado == 'En proceso'">
             <template v-slot:activator="{ on, attrs }">
               <v-btn
                 class="mx-2"
@@ -158,15 +159,34 @@
                 color="primary"
                 v-bind="attrs"
                 v-on="on"
-                @click="abrirDialogo(item.productos, item.estado_pago)"
+                @click="abrirDialogoRegistrar(item)"
+              >
+                <v-icon dark left>
+                  mdi-upload
+                </v-icon>
+                <span>Subir resultado</span>
+              </v-btn>
+            </template>
+            <span>Visualice los productos del pedido</span>
+          </v-tooltip>
+          <v-tooltip top v-if="item.procedimientos.estado == 'Subido'">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                class="mx-2"
+                small
+                dark
+                color="primary"
+                v-bind="attrs"
+                v-on="on"
+                @click="abrirDialogoVisualizar(item)"
               >
                 <v-icon dark left>
                   mdi-card-search
                 </v-icon>
-                <span>Detalle</span>
+                <span>Visualizar resultado</span>
               </v-btn>
             </template>
-            <span>Visualice los productos del pedido</span>
+            <span>Visualice el resultado subido</span>
           </v-tooltip>
               </template>
               </v-data-table>
@@ -196,9 +216,16 @@
     <v-dialog
         transition="dialog-bottom-transition"
         v-model="abrirDialogoDetalle"
-        max-width="1200px"
+        max-width="700px"
     >
-    <DetallePendientes :listaproductos = listaProductos  :estado = estado @emit-cerrar-dialog ="cerrarDialogo()"></DetallePendientes>
+    <SubirResultExamenAux :resultadoObjToAgregar="resultadoObjToAgregar" :userId="idusuario" :objOrdenes="objOrdenes" @emit-close-dialog="cerrarDialogoRegistrar" @emit-recargar-tablas="recargaTablas"></SubirResultExamenAux>
+    </v-dialog>
+    <v-dialog
+        transition="dialog-bottom-transition"
+        v-model="abrirDialogoVisua"
+        max-width="700px"
+    >
+    <ConsultarResultExamenAux :infoResultExamenAuxiliar="resultadoDetalle" @emit-close-dialog="cerrarDialogoVisualizar"></ConsultarResultExamenAux>
     </v-dialog>
     <v-dialog width="450px" v-model="cargaRegistro" persistent>
         <v-card height="300px">
@@ -215,16 +242,45 @@
            <v-card-subtitle class="justify-center" style="font-weight:bold;text-align:center">En unos momentos finalizaremos...</v-card-subtitle>
         </v-card>
       </v-dialog>
+      <v-dialog v-model="dialogConfirmacion" max-width="500">
+        <v-card>
+          <v-card-title class="label-title">
+            <h3>¿Desea confirmar la atención?</h3>          
+          </v-card-title>
+          <v-card-text>
+            Una vez confirmada la atención podra subir los resultados del examen
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="red darken-1"
+              text
+              @click="dialogConfirmacion = false"
+            >
+              Cancelar
+            </v-btn>
+
+            <v-btn
+              color="green darken-1"
+              text
+              @click="atender()"
+            >
+              Aceptar
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
   </v-card>
 </template>
 <script>
 import axios from "axios"
-import DetallePendientes from "@/components/Pedidos/DetallePendientes"
-import { mapActions,mapGetters } from "vuex";
+import SubirResultExamenAux from "@/components/Resultados/OperDialogs/ResultadosExamenAux/SubirResultExamenAux";
+import ConsultarResultExamenAux from "@/components/Resultados/OperDialogs/ResultadosExamenAux/ConsultarResultExamenAux";
 export default {
 name: "ExamenesPorPaciente",
 components: {
-    DetallePendientes,
+    SubirResultExamenAux,
+    ConsultarResultExamenAux
 },
 props: {
     idusuario: {
@@ -233,35 +289,48 @@ props: {
     idpaciente: {
       type: String,
     },
+    idmedico: {
+      type: String,
+    },
 },
 data(){
     return{
      usuario:"", 
      showInfo:false,
      page: 1,
-    pageCount: 0,
-    abrirDialogoDetalle : false,
-    abrirDialogoDetalle : false,
-    estado:"",
-    cargaRegistro : false,
+     pageCount: 0,
+     abrirDialogoDetalle : false,
+     abrirDialogoVisua : false,
+     dialogConfirmacion: false,
+     estado:"",
+     cargaRegistro : false,
      listaPedidos:[],
      listaProductos:[],
+     listaOrdenes:[],
+     listaTurnos:[],
+     listExamenes:[],
+     examenUnico: "",
      headerPedidos: [
         {
-          text: "Fecha de compra",
-          value: "fecha_creacion",
+          text: "Examen a realizar",
+          value: "datosexamen.descripcion",
           sortable: false,
           align: "start",
         },
         {
-          text: "Estado del pedido",
+          text: "Estado del examen",
           sortable: false,
-          value: "estado_pago",
+          value: "procedimientos.estado",
         },
         {
-          text: "Precio total",
+          text: "Hora de atención",
           sortable: false,
-          value: "precio_neto",
+          value: "hora",
+        },
+        {
+          text: "Fecha de atención",
+          sortable: false,
+          value: "fecha",
         },
         {
           text: "Acciónes",
@@ -270,15 +339,28 @@ data(){
           align: "center",
         },
       ],
+      objOrdenes:{
+        id_orden: "",
+        id_examen: "",
+        id_turno_orden: "",
+        estado:"subido",
+      },
+      resultadoObjToAgregar:{
+        codigo: "",
+        nombre: "",
+        tipo: "",
+      },
+      resultadoDetalle:{
+
+      }
     };
 },
 async created(){
     this.showInfo = true,
     await this.obtenerusuario();
     await this.obtenerPaciente();
-    await this.obtenerPedidos();
-    this.showInfo = false,
-    console.log(this.usuario)
+    await this.obtenerExamenes();
+    this.showInfo = false
 },
 methods:{
   async obtenerusuario() {
@@ -297,7 +379,46 @@ methods:{
           })
           .catch((err) => console.log(err));
   },
-  async obtenerPedidos() {
+  async obtenerExamenes() {
+      await this.obtenerTurnosMedicos();
+      await this.obtenerOrdenes();
+      for(var i = 0; i < this.listaTurnos.length; i++){
+        for(var j = 0; j < this.listaOrdenes.length; j++){
+          if(this.listaTurnos[i].id == this.listaOrdenes[j].procedimientos.id_turno_orden){
+            var examen = this.listaOrdenes[j];
+            examen.id = examen.id + "-" + j
+            examen.procedimientos.estado = this.capitalizarPrimeraLetra(examen.procedimientos.estado);
+            for(var k = 0; k < this.listaTurnos[i].cupos.length; k++){
+              if(this.listaTurnos[i].cupos[k].id_examen == this.listaOrdenes[j].procedimientos.id_examen && this.listaTurnos[i].cupos[k].paciente == this.idpaciente){
+                examen.fecha = this.obtenerFecha(this.listaTurnos[i].cupos[k].hora_inicio);
+                examen.hora = this.obtenerHora(this.listaTurnos[i].cupos[k].hora_inicio);
+              }
+            }
+            this.listExamenes.push(examen);
+          }
+        }
+      }
+      console.log("Estos son mi lista de Examnes")
+      console.log(this.listExamenes)
+  },
+  async obtenerOrdenes() {
+      await axios
+      .get("/Ordenes/ordenesbypaciente?id_paciente="+this.idpaciente)
+      .then((x) => {
+        this.listaOrdenes = x.data;
+      })
+      .catch((err) => console.log(err));
+  },
+  async obtenerTurnosMedicos() {
+      await axios
+      .get("/Turno_Orden/GetByIdMedico?id="+this.idmedico)
+      .then((x) => {
+        this.listaTurnos = x.data;
+        console.log(this.listaTurnos);
+      })
+      .catch((err) => console.log(err));
+  },
+  /*async obtenerPedidos() {
       await axios
           .get("/Pedidos/byidpacientepedidospendiente?id_paciente="+this.idpaciente)
           .then((x) => {
@@ -306,7 +427,7 @@ methods:{
             console.log(this.listaPedidos)
           })
           .catch((err) => console.log(err));
-  },
+  },*/
   abrirDialogo(productos,estado){
       this.listaProductos = [],
       this.estado = "",
@@ -318,17 +439,80 @@ methods:{
       this.listaProductos = [],
       this.abrirDialogoDetalle = false;
   },
-  async atenderPedido(pedido){
+  async abrirDialodoConfirmar(pedido){
+    this.examenUnico = pedido,
+    this.dialogConfirmacion = true
+  },
+  obtenerFecha(fecha){
+    return new Date(fecha).toLocaleDateString('ja-JP', { year: 'numeric', month: '2-digit', day: '2-digit'}).replace(/\//gi,'-');
+  },
+  obtenerHora(fecha){
+    return fecha.substr(11, 5);
+  },
+  capitalizarPrimeraLetra(str) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+  },
+  async atender(){
     this.cargaRegistro = true;
-    pedido.estado_pago = "en proceso"
+    this.listExamenes = [];
     await axios
-        .put("/Pedidos", pedido)
-        .then((res) => {
-        })
-        .catch((err) => console.log(err));
-    await this.obtenerPedidos();
+      .put("/Ordenes/modificarestado/"+ this.examenUnico.id.substr(0,24) + "/" + this.examenUnico.procedimientos.id_examen + "/"+ this.examenUnico.procedimientos.id_turno_orden + "/" + "en proceso" + "/" + "fake")
+      .then((x) => {
+        console.log(x.data)
+      })
+      .catch((err) => console.log(err));
+    await this.obtenerExamenes();
+    this.dialogConfirmacion = false;
     this.cargaRegistro = false;
-  }
+  },
+  abrirDialogoRegistrar(orden){
+    console.log("No me carga mis cosas ayuda"),
+    console.log(orden);
+    this.objOrdenes.id_orden = orden.id.substr(0,24),
+    this.objOrdenes.id_examen = orden.procedimientos.id_examen,
+    this.objOrdenes.id_turno_orden = orden.procedimientos.id_turno_orden,
+    this.resultadoObjToAgregar.codigo = orden.procedimientos.id_examen,
+    this.resultadoObjToAgregar.nombre = orden.datosexamen.descripcion,
+    this.resultadoObjToAgregar.tipo = "Examen",
+    this.abrirDialogoDetalle = true;
+  },
+  cerrarDialogoRegistrar(){
+      this.objOrdenes = {
+        id_orden: "",
+        id_examen: "",
+        id_turno_orden: "",
+        estado:"subido",
+      },
+      this.resultadoObjToAgregar ={
+        codigo: "",
+        nombre: "",
+        tipo: "",
+      }
+      this.abrirDialogoDetalle = false;
+  },
+  async recargaTablas(){
+    this.listExamenes = [];
+    await this.obtenerExamenes();
+  },
+  async abrirDialogoVisualizar(orden){
+    await this.obtenerResultado(orden.procedimientos.id_resultado_examen)
+    this.abrirDialogoVisua = true;
+  },
+  async obtenerResultado(id_resultado){
+    await axios
+      .get("/ResultadoExamen/id?id="+id_resultado)
+      .then((x) => {
+        this.resultadoDetalle = x.data;
+        console.log(this.listaTurnos);
+      })
+      .catch((err) => console.log(err));
+  },
+  cerrarDialogoVisualizar(){
+    this.resultadoDetalle = {
+
+    },
+    this.abrirDialogoVisua = false;
+  },
 },
 computed:{
 },
